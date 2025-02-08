be951e7b966a6d1af73a46c0fefa048cb001e4fd
Francis <francism4402@gmail.com>
2025-02-09 03:41:11 +0600

    base link update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ import { toast } from "sonner";
 import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://book-shop-green.vercel.app/',
+    baseUrl: 'https://book-shop-green.vercel.app/api/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
